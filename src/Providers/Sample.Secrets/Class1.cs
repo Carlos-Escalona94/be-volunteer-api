11dcf6208687f25404fc97324ed5c981284e2b8d
Carlos-Escalona94 <ing.carlosuc@gmail.com>
2023-01-07 09:51:52 -0300

    a

--- src/Providers/Sample.Secrets/Class1.cs
+++ src/Providers/Sample.Secrets/Class1.cs
@@ -1,5 +1,8 @@
-﻿namespace Sample.Secrets;
-public class Class1
-{
+﻿using Sample.Auth.Interfaces;
 
+namespace Sample.Secrets;
+public class LocalSecretProvider : ISecretProvider
+{
+    public string ClientId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string ClientSecret { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 }
